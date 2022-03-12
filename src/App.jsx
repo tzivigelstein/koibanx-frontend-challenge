5d2fe8cdce9bc2476436ece6763f997e057f7f3f
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-03-12 11:17:13 -0300

    Add get stores on load

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { useEffect } from 'react'
+
+import useApp from './hooks/useApp'
+
 import Navbar from './components/Navbar'
 import Table from './components/Table'
 
 function App() {
+  const { page, getStores, stores } = useApp()
+
+  useEffect(() => {
+    getStores()
+  }, [page])
+
   return (
     <div className="App">
       <Navbar />
-      <Table />
+      <Table stores={stores} />
     </div>
   )
 }
