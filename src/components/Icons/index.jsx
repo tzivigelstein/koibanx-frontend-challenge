ce8c5c07a970853dbcc6f30363b561fbe9f75019
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-03-12 12:58:51 -0300

    Create Trash and Arrow icons

---
+++ src/components/Icons/index.jsx
@@ -0,0 +1,43 @@
+export const Trash = props => (
+  <svg
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+    stroke="currentColor"
+    strokeWidth="2"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <polyline points="3 6 5 6 21 6"></polyline>
+    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+  </svg>
+)
+
+export const Arrow = ({ dir = up, ...props }) => {
+  const DIRECTIONS = {
+    down: { transform: 'rotate(180deg)' },
+    left: { transform: 'rotate(90deg)' },
+    right: { transform: 'rotate(-90deg)' },
+    up: {}
+  }
+
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      stroke="currentColor"
+      strokeWidth="2"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      style={DIRECTIONS[dir]}
+      {...props}
+    >
+      <line x1="12" y1="19" x2="12" y2="5"></line>
+      <polyline points="5 12 12 5 19 12"></polyline>
+    </svg>
+  )
+}
