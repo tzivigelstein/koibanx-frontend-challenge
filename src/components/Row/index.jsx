99453881ce74ffa4dd14bcf4e9346fdfa27405c8
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-03-12 13:03:53 -0300

    Add highlight component

--- src/components/Row/index.jsx
+++ src/components/Row/index.jsx
@@ -1,11 +1,15 @@
+import useApp from '../../hooks/useApp'
+import TextHighlight from '../TextHighlight'
+
 export default function Row({ store }) {
+  const { searchTerm, loading, highlight } = useApp()
   const { _id, storeName, CUIT, concepts, currentBalance, active, lastTransaction } = store
 
   return (
     <tr>
-      <td>{_id}</td>
-      <td>{storeName}</td>
-      <td>{CUIT}</td>
+      <td>{highlight && !loading ? <TextHighlight text={_id} shouldBeHighlighted={searchTerm} /> : _id}</td>
+      <td>{highlight && !loading ? <TextHighlight text={storeName} shouldBeHighlighted={searchTerm} /> : storeName}</td>
+      <td>{highlight && !loading ? <TextHighlight text={CUIT} shouldBeHighlighted={searchTerm} /> : CUIT}</td>
       {concepts.map(concept => {
         return <td key={crypto.randomUUID()}>{concept}</td>
       })}
