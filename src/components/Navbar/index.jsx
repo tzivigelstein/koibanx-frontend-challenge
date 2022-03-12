25b52680fc3e409fa7745c06bb665ce09f35a35c
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-03-12 12:16:35 -0300

    Add active filter handling

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,12 +1,24 @@
 import styles from './index.module.css'
 
+const STORE_STATUSES = {
+  1: 'ACTIVE',
+  0: 'INACTIVE',
+  null: 'ALL'
+}
+
 export default function Navbar() {
+  const { storeStatus, setActiveFilter } = useContext(StoreContext)
+
+  function handleActiveFilter(e) {
+    setActiveFilter(e.target.value)
+  }
+
   return (
     <>
       <nav className={styles.navbar}>
         <div className={styles.searchBarContainer}>
           <input className={styles.input} type="text" placeholder="Buscar..." />
-          <select value="" className={styles.select}>
+          <select onChange={handleActiveFilter} value={STORE_STATUSES[storeStatus]} className={styles.select}>
             <option value="ALL">Todos</option>
             <option value="ACTIVE">Activo</option>
             <option value="INACTIVE">Inactivo</option>
