ef5c2d524fdab3c94860b818e514be0815787666
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-03-12 10:53:38 -0300

    Add Table component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from './components/Navbar'
+import Table from './components/Table'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Table />
     </div>
   )
 }
